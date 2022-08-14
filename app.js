9970095fbe474355a120d85804186a883b7225eb
Charlotte Bilsing <Charlotte.Bilsing@posteo.de>
2022-08-14 19:47:25 +0200

    Log each page request using express-request-audit library and write to file

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const cors = require('cors');
 var express = require('express');
 var path = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 var logger = require('morgan');
-const db = require('./model/queries')
+var audit = require('express-request-audit');
+const db = require('./model/queries');
+
+const fs = require('fs').promises; // Import the promise-based version of the fs library
 
 var app = express();
 
@@ -13,21 +16,43 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.json())
+// log every page request using express-request-audit library
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-)
+  audit({
+    auditor: async function (event) {
+      // write following data for each page request in file
+      let auditData = `timestamp: ${event.response.timestamp} \n method: ${event.request.method} \n URL: ${event.request.url} \n status_code: ${event.response.status_code} \n header-randomValue: ${event.response.headers.random}\n\n`;
+
+      try {
+        await fs.appendFile('requestAudit.txt', auditData);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    request: {
+      maxBodyLength: 50, // limit length to 50 chars + '...'
+    },
+    response: {
+      maxBodyLength: 50, // limit length to 50 chars + '...'
+    },
+  })
+);
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.get('/', (request, response) => {
-    response.json({
-        info: 'Node.js, Express, and Postgres API'
-    })
-})
-app.get('/products', db.getProducts)
-app.get('/products/:id', db.getProductById)
-app.post('/products', db.createProduct)
-app.put('/products/:id', db.updateProduct)
-app.delete('/products/:id', db.deleteProduct)
+  response.json({
+    info: 'Node.js, Express, and Postgres API',
+  });
+});
+app.get('/products', db.getProducts);
+app.get('/products/:id', db.getProductById);
+app.post('/products', db.createProduct);
+app.put('/products/:id', db.updateProduct);
+app.delete('/products/:id', db.deleteProduct);
 
 module.exports = app;
